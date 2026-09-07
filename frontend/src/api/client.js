b36ff95c01b7d79127e46106ed0c4e27af68cab2
Thuokariuki <samcreations104@gmail.com>
2026-09-07 11:23:55 +0300

    Update API base URL to production endpoint

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://mkulimahub-b53g.onrender.com",
 });
 
 client.interceptors.request.use((config) => {
