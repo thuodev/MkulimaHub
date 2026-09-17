fd240c717d83601b16216817859ae6715c20ef36
Thuokariuki <samcreations104@gmail.com>
2026-09-17 10:52:13 +0300

    Add DNS configuration for IPv4 preference and fix typo in mailer connection timeout

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -12,7 +12,7 @@ const transporter = nodemailer.createTransport({
     pass: process.env.GMAIL_APP_PASSWORD,
   },
   family: 4, // Use IPv4
-  connectTimeout: 10000, // 10 seconds
+  connectionTimeout: 10000, // 10 seconds
 });
 
 export const sendEmployeeCredentialsEmail = async (
